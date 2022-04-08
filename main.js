4b2f956214be7ddd2e4d9ce42b06fbc9b326da79
sadbc1235 <sadbc1235@naver.com>
2022-04-08 22:16:31 +0900

    complet all UI

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-const articles = document.querySelectorAll('article');
-const aside = document.querySelector('aside');
-const btnClose = aside.querySelector('.btnClose');
+const articles = document.querySelectorAll("article");
+const aside = document.querySelector("aside");
+const btnClose = aside.querySelector(".btnClose");
 
-articles.forEach(article => {
-	article.addEventListener('mouseenter', e => {
-		e.currentTarget.querySelector('video').play();
-	})
+articles.forEach((article) => {
+  article.addEventListener("mouseenter", (e) => {
+    e.currentTarget.querySelector("video").play();
+  });
 
-	article.addEventListener('mouseleave', e => {
-		e.currentTarget.querySelector('video').pause();
-	})
+  article.addEventListener("mouseleave", (e) => {
+    e.currentTarget.querySelector("video").pause();
+  });
 
-	article.addEventListener('click', e => {
-		const tit = e.currentTarget.querySelector('h2').innerText;
-		const con = e.currentTarget.querySelector('p').innerText;
-		const vidSrc = e.currentTarget.querySelector('video').getAttribute('src');
+  article.addEventListener("click", (e) => {
+    const tit = e.currentTarget.querySelector("h2").innerText;
+    const con = e.currentTarget.querySelector("p").innerText;
+    const vidSrc = e.currentTarget.querySelector("video").getAttribute("src");
 
-		aside.querySelector('h2').innerText = tit;
-		aside.querySelector('p').innerText = con;
-		aside.querySelector('video').setAttribute('src', vidSrc);
-		aside.classList.add('on');
-	})
+    aside.querySelector("h2").innerText = tit;
+    aside.querySelector("p").innerText = con;
+    aside.querySelector("video").setAttribute("src", vidSrc);
+    aside.classList.add("on");
+  });
 
-	btnClose.addEventListener('click', ()=>{
-		aside.classList.remove('on');
-	})
-
-})
+  btnClose.addEventListener("click", () => {
+    aside.classList.remove("on");
+  });
+});
